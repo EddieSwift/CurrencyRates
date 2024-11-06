eb84aad2532fc1d1eb6cf59ba69d19be27142c2c
Eduard Galchenko <eddiegalchenko@gmail.com>
2024-11-06 14:31:22 +0100

    Initial Commit

---
+++ CurrencyRates/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CurrencyRates
+//
+//  Created by Eduard Galchenko on 06.11.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CurrencyRates/CurrencyRatesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyRatesApp.swift
+//  CurrencyRates
+//
+//  Created by Eduard Galchenko on 06.11.24.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyRatesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
